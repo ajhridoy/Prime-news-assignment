efb5fe74a0e5ca6c36efa816ef47a5ee23156672
ajhridoy <ajhridoy461@gmail.com>
2022-09-03 01:42:10 +0600

    display category name

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello')
+const loadCategory = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => displayCategory(data.data.news_category))
+}
+const displayCategory = categoris => {
+    // console.log(categoris)
+    const disCategory = document.getElementById('dis-category');
+    categoris.forEach(category => {
+        console.log(category)   
+        const div = document.createElement('div');
+        div.classList.add('hover-back')
+        div.innerText = category.category_name
+        disCategory.appendChild(div);
+    });
+}
+loadCategory()
